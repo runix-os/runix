95e022192578d1d752bf9ece3fc14973c677f538
Callum Ward <wards.callum@gmail.com>
2019-05-05 15:46:33 +0100

    Update the TTY handler to handle newlines and scroll

--- kernel/arch/i386/tty.c
+++ kernel/arch/i386/tty.c
@@ -16,43 +16,75 @@ static size_t terminal_column;
 static uint8_t terminal_color;
 static uint16_t* terminal_buffer;
 
+void terminal_clear(void) {
+    for (size_t y = 0; y < VGA_HEIGHT; y++) {
+        for (size_t x = 0; x < VGA_WIDTH; x++) {
+            const size_t index = y * VGA_WIDTH + x;
+            terminal_buffer[index] = vga_entry(' ', terminal_color);
+        }
+    }
+}
+
 void terminal_initialize(void) {
-	terminal_row = 0;
-	terminal_column = 0;
-	terminal_color = vga_entry_color(VGA_COLOR_LIGHT_GREY, VGA_COLOR_BLACK);
-	terminal_buffer = VGA_MEMORY;
-	for (size_t y = 0; y < VGA_HEIGHT; y++) {
-		for (size_t x = 0; x < VGA_WIDTH; x++) {
-			const size_t index = y * VGA_WIDTH + x;
-			terminal_buffer[index] = vga_entry(' ', terminal_color);
-		}
-	}
+    terminal_row = 0;
+    terminal_column = 0;
+    terminal_color = vga_entry_color(VGA_COLOR_WHITE, VGA_COLOR_BLACK);
+    terminal_buffer = VGA_MEMORY;
+    terminal_clear();
 }
 
 void terminal_setcolor(uint8_t color) {
-	terminal_color = color;
+    terminal_color = color;
 }
 
 void terminal_putentryat(unsigned char c, uint8_t color, size_t x, size_t y) {
-	const size_t index = y * VGA_WIDTH + x;
-	terminal_buffer[index] = vga_entry(c, color);
+    const size_t index = y * VGA_WIDTH + x;
+    terminal_buffer[index] = vga_entry(c, color);
+}
+
+void terminal_scroll(size_t lines) {
+    if (lines >= VGA_HEIGHT) {
+        terminal_clear();
+    } else {
+        /* Start at the line which will move off the screen */
+        for (size_t y = lines; y < VGA_HEIGHT; y++) {
+            for (size_t x = 0; x < VGA_WIDTH; x++) {
+		const size_t old_index = y * VGA_WIDTH + x;
+		const size_t new_index = (y - lines) * VGA_WIDTH + x;
+                terminal_buffer[new_index] = terminal_buffer[old_index];
+            }
+        }
+        /* Wipe all the lines that got shifted up */
+        for (size_t y = VGA_HEIGHT - lines; y < VGA_HEIGHT; y++) {
+            for (size_t x = 0; x < VGA_WIDTH; x++) {
+                const size_t index = y * VGA_WIDTH + x;
+                terminal_buffer[index] = vga_entry(' ', terminal_color);
+            }
+        }
+    }
 }
 
 void terminal_putchar(char c) {
-	unsigned char uc = c;
-	terminal_putentryat(uc, terminal_color, terminal_column, terminal_row);
-	if (++terminal_column == VGA_WIDTH) {
-		terminal_column = 0;
-		if (++terminal_row == VGA_HEIGHT)
-			terminal_row = 0;
-	}
+    unsigned char uc = c;
+    if (terminal_column == VGA_HEIGHT || uc == '\n') {
+        terminal_column = 0;
+        terminal_row++;
+        if (terminal_row == VGA_HEIGHT) {
+            terminal_row--;
+            terminal_scroll(1);
+        }
+    }
+    if (uc != '\n') {
+        terminal_putentryat(uc, terminal_color, terminal_column, terminal_row); 
+        terminal_column++;
+    }
 }
 
 void terminal_write(const char* data, size_t size) {
-	for (size_t i = 0; i < size; i++)
-		terminal_putchar(data[i]);
+    for (size_t i = 0; i < size; i++)
+        terminal_putchar(data[i]);
 }
 
 void terminal_writestring(const char* data) {
-	terminal_write(data, strlen(data));
+    terminal_write(data, strlen(data));
 }
